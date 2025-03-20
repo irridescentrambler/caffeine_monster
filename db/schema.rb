fb2ee3580f73da4ed36a1bfd880c043411f9a95f
Nikhil Mohadikar <nikhil.mohadikar@coupa.com>
2025-03-20 16:36:52 +0530

    Fixes the example 2 issue.

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_21_134309) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end

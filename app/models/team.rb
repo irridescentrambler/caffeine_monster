78b52e9f66326ddf35fa399db32efe7d1d98afc2
Nikhil Mohadikar <nikhil.mohadikar@coupa.com>
2025-03-21 19:55:12 +0530

    Adds membership association.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# Deals with teams table
 class Team < ApplicationRecord
+  has_many :memberships
+  has_many :members, through: :memberships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@
 class User < ApplicationRecord
   has_one :account_user
   has_one :account, through: :account_user
+  has_many :memberships, foreign_key: :member_id
+  has_and_belongs_to_many :teams, join_table: :memberships, foreign_key: :member_id
 
   validates_uniqueness_of :email
 end

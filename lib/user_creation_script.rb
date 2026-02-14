5e5db3beb1fb9c8dafe3e56ef114d2057a1b9c6a
Nikhil Mohadikar <mohadikar59@gmail.com>
2026-02-14 14:50:49 +0530

    Fixes broken production build

--- lib/user_creation_script.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'net/http'
-require 'uri'
-
-def create_user(name, email)
-  uri = URI('http://localhost:3000/users')
-  Net::HTTP.post_form(uri, 'user[email]' => email, 'user[name]' => name)
-end
